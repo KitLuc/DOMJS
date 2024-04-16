ed0f31117e819cb5d8fb617b43596f3261efbb1f
KitLuc <tobongoomez809@gmail.com>
2024-04-15 22:52:07 -0500

    Change code in file

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,8 +1,8 @@
-import FormComponent from './component/formComponent.js';
-import H2Component from './component/h2Component.js';
-import LabelComponent from './component/labelComponent.js';
-import InputComponent from './component/inputComponent.js';
-import ButtonComponent from './component/buttonComponent.js';
+import FormComponent from './component/form.component.js';
+import H2Component from './component/h2.component.js';
+import LabelComponent from './component/label.component.js';
+import InputComponent from './component/input.component.js';
+import ButtonComponent from './component/button.component.js';
 
 
 export default function login() {
@@ -17,18 +17,26 @@ export default function login() {
     const labelPassword = new LabelComponent('password', 'Contraseña');
     const inputPassword = new InputComponent('password', 'Contraseña');
     inputPassword.element.classList.add('w-full', 'px-3', 'py-2', 'border', 'rounded-md', 'focus:outline-none', 'focus:border-blue-500');
-    
+
     const button = new ButtonComponent({
-        type: 'submit',
+        type: 'click',
         text: 'Iniciar sesión',
         onClickHandler: () => {
-            console.log('Iniciando sesión...');
-        },
-        typeComportament: 'submit'
+            const username = inputUser.element.value;
+            const password = inputPassword.element.value;
+
+            if (username === 'admin@gmail.com' && password === 'admin')
+                window.location.replace('drag_drop.html');
+            else
+                alert('Usuario o contraseña incorrectos');
+
+        }
     });
-    button.element.classList.add('bg-blue-500', 'text-white', 'px-4', 'py-2', 'rounded-md', 'hover:bg-blue-600', 'focus:outline-none', 'focus:bg-blue-600', 'w-full');
-    
-    const form = FormComponent({id: 'login', method: 'POST'},
-                                [h2, labelUser, inputUser, labelPassword, inputPassword, button]);
+
+    button.element.classList.add('bg-blue-500', 'text-center', 'text-white', 'px-4', 'py-2', 'rounded-md', 'hover:bg-blue-600', 'focus:outline-none', 'focus:bg-blue-600', 'w-full');
+
+    const form = FormComponent({ id: 'login', method: 'POST' },
+        [h2, labelUser, inputUser, labelPassword, inputPassword, button]);
+        form.classList.add('flex', 'flex-col', 'justify-center', 'max-w-md', 'mx-auto');
     return form;
 }
