8ec9a68d3c262ed5408fe69668af7b85965ddc21
KitLuc <tobongoomez809@gmail.com>
2024-04-15 22:52:52 -0500

    Delete name structure in files name

--- public/assets/js/documentComponent.js
+++
@@ -1,9 +0,0 @@
-import login from './login.js';
-import mainComponent from './component/mainComponent.js';
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const form = login();
-    const main = mainComponent([form]);
-    document.body.appendChild(main);
-});
